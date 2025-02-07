199e45445c2b71c8ccdbf09fd46561297044d752
Matilde Morrone <contact@morrone.dev>
2025-02-07 12:57:25 +0100

    Rewrite filetree using solid and unplugin-icons

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import solidJs from '@astrojs/solid-js';
 import node from '@astrojs/node';
+import Icons from 'unplugin-icons/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
   }),
   integrations: [icon(), solidJs()],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      Icons({
+        compiler: 'solid',
+      }),
+    ],
   },
 });
